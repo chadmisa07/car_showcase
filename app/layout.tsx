08895f097399370ec534944401f9cc4b18481281
Chad Misa <chad@matcherino.com>
2024-01-18 12:33:23 +0800

    Meta and imaga_src

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width"></meta>
+      <meta name="title" content="Car Hub"></meta>
+      <meta
+        name="description"
+        content="Find, book or rent a car -- quickly and easily!"
+      ></meta>
+      <meta
+        name="keywords"
+        content="Car, Car Show Case, Car Rental, Car Hub"
+      ></meta>
+      <meta name="author" content="Chat Misa"></meta>
+      <link rel="image_src" href="../public/hero.png" />
       <body className="relative">
         <Navbar />
         {children}
